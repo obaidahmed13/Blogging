a48c2c6fb2d01acea5a7af3ddc57a85ab56bce27
Obaid Ahmed <138173339+obaidahmed13@users.noreply.github.com>
2024-06-18 12:09:07 -0400

    Controller

--- src/main/java/genspark/blogging/Blogging/controllers/UserController.java
+++ src/main/java/genspark/blogging/Blogging/controllers/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
--- src/test/java/genspark/blogging/Blogging/BloggingApplicationTests.java
+++ src/test/java/genspark/blogging/Blogging/BloggingApplicationTests.java
@@ -1,13 +1,70 @@
 package genspark.blogging.Blogging;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+import java.util.Arrays;
+import java.util.List;
+
+import genspark.blogging.Blogging.controllers.PostController;
+import genspark.blogging.Blogging.models.Post;
+import genspark.blogging.Blogging.services.PostServiceImp;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(PostController.class)
 class BloggingApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private PostServiceImp postServ;
+
 	@Test
-	void contextLoads() {
+	public void createPost() throws Exception {
+		Post post = new Post();
+		post.setTitle("Test Title");
+		post.setContent("Test Content");
+		post.setAuthor("Test Author");
+
+		when(postServ.createPost(any(Post.class))).thenReturn(post);
+
+		mockMvc.perform(post("/post/add")
+						.with(user("user").roles("NORMAL")) // or authorities(() -> "NORMAL")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content("{\"title\": \"Test Title\", \"content\": \"Test Content\", \"author\": \"Test Author\"}"))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	public void getAllApproved() throws Exception {
+		List<Post> posts = Arrays.asList(new Post(), new Post());
+		when(postServ.getAllApprovedPosts()).thenReturn(posts);
+
+		mockMvc.perform(get("/post/approved")
+						.with(user("user").roles("NORMAL"))) // Correct usage for setting roles
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	public void getAllUnapproved() throws Exception {
+		List<Post> posts = Arrays.asList(new Post(), new Post());
+		when(postServ.getAllApprovedFalse()).thenReturn(posts);
+
+		mockMvc.perform(get("/post/unapproved")
+						.with(user("admin").roles("ADMIN"))) // Correct usage for setting roles
+				.andExpect(status().isOk());
 	}
 
 }
